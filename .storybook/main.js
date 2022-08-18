c5fa4160d12f4c6261dc422f092235b097d2b6a2
Aaron Moore <hi@madebyaaron.xyz>
2022-08-18 22:30:14 +0100

    set up storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,27 @@
+const path = require("path")
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    { // include this if you're using tailwind / postcss
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  webpackFinal: async (config) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+
+    return config;
   }
 }
